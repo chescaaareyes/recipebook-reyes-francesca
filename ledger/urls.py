653a4afd7f3e08426388b46dc965f13014023297
Chesca Reyes <francesca.dominique.reyes@student.ateneo.edu>
2024-02-15 20:19:09 +0800

    Add recipe 1 page and content

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import recipes_list
+from .views import recipes_list, recipe_1
 
 urlpatterns = [
   path('recipes/list', recipes_list, name="list"),
+  path('recipe/1', recipe_1, name="recipe1"),
 ]
 
 app_name = "ledger"
