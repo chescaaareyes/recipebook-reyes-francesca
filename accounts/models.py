6704b0df922dd6f8b5f83445a9fca05655047bdc
Francesca Reyes <francesca.dominique.reyes@student.ateneo.edu>
2024-03-15 09:47:07 +0800

    Create Profile Model and Profile Model Admin

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=500, validators=[MinLengthValidator(255)])
